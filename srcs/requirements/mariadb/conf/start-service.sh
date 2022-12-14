c8209b93b64075003fbfe290b9a20e58692c0b4c
Suhyeon AHN <suahn@e1r8p17.clusters.42paris.fr>
2022-12-14 21:15:43 +0100

    works well on vm

--- srcs/requirements/mariadb/conf/start-service.sh
+++ srcs/requirements/mariadb/conf/start-service.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 #SETUP DATABASE
-#service mysql start
 echo "CREATE DATABASE IF NOT EXISTS db;" | mysql -u root
 echo "CREATE USER IF NOT EXISTS 'wp'@'%' IDENTIFIED BY 'pass';" | mysql -u root
 echo "GRANT ALL ON db.* TO 'wp'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-#test
-bash test.sh
-#mysql user gains access to the data files
-#mysqld
