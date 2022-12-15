af90b71d2d1a901a16b2102206ca7e90282e634b
Suhyeon AHN <suahn@e2r2p9.clusters.42paris.fr>
2022-12-15 17:56:28 +0100

    rm unnecessary lines

--- srcs/requirements/mariadb/conf/start-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#SETUP DATABASE
-echo "CREATE DATABASE IF NOT EXISTS db;" | mysql -u root
-echo "CREATE USER IF NOT EXISTS 'wp'@'%' IDENTIFIED BY 'pass';" | mysql -u root
-echo "GRANT ALL ON db.* TO 'wp'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
