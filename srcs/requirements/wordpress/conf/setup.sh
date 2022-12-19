fd6a1982a50563cb183141ef31e23e68d7936bc5
Suhyeon AHN <suahn@e1r10p5.clusters.42paris.fr>
2022-12-19 19:52:21 +0100

    wordpress setup fix

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -8,6 +8,6 @@ if [! -f "$WP-CONFIG"]; then
     #create the wp-config.php file
     wp core config --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX
     #wp db create
-    wp core install --url="https://suahn.42.fr" --title="suahn's home" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email="suahn@student.42.fr"
-    wp user create --user_login=$WORDPRESS_DEFAULT_USE --user_pass=$WORDPRESS_DEFAULT_PASSWORD --role="Default"
+    wp core install --url="https://suahn.42.fr" --title="home" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email="https://agentsuahn.42.fr"
+    wp user create $WORDPRESS_USER "https://suahn.42.fr" --user_pass=$WORDPRESS_USER_PASS --role=Default
 fi
