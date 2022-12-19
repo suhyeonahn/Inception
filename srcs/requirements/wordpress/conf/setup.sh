e0dc0b5b845f1571c2ea3142fd7425b5318f4a24
Suhyeon AHN <suahn@e1r10p5.clusters.42paris.fr>
2022-12-19 20:28:31 +0100

    rm useless lines

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -8,6 +8,6 @@ if [! -f "$WP-CONFIG"]; then
     #create the wp-config.php file
     wp core config --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX
     #wp db create
-    wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email="https://agentsuahn.42.fr"
+    wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD
     wp user create $WORDPRESS_USER "https://suahn.42.fr" --user_pass=$WORDPRESS_USER_PASS --role=Default
 fi
