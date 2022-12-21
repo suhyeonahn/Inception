b0a4fb21c1998b6a6c73330988f1cc82800ecc45
Suhyeon AHN <suahn@e1r11p3.clusters.42paris.fr>
2022-12-21 18:34:38 +0100

    fix wordpress script

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -6,7 +6,7 @@ if  [ ! -f "$WP_CONFIG" ]; then
     #download the latest version of the WordPress core files.
     wp core download --allow-root
     #create the wp-config.php file
-    wp core config --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --allow-root
+    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --allow-root
     #wp db create
     wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
     wp user create $WORDPRESS_USER "https://suahn.42.fr" --user_pass=$WORDPRESS_USER_PASS --role=Default --allow-root
