ca05a23c1f0a6b230dd30d97fa6c340c736c94aa
Suhyeon AHN <suahn@e1r11p3.clusters.42paris.fr>
2022-12-21 23:21:23 +0100

    fix err

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-WP_USER= "/var/www/html/users"
-if  [ ! -f "$WP_USER" ]; then
-    wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
-    wp user create $WORDPRESS_USER "https://suahn.42.fr" --user_pass=$WORDPRESS_USER_PASS --role=Default --allow-root
-    touch users
+WP_CONFIG= "/var/www/html/WP_CONFIG"
+if  [ ! -f "$WP_CONFIG" ]; then
+    wp core install --url=$WORDPRESS_URI --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$suahn@student.42.fr --allow-root
+    wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=Default --allow-root
+    touch WP_CONFIG
 fi
