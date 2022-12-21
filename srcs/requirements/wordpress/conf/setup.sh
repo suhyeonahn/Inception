7779a313ce7ad00313007def2ede3ed7e3de327d
Suhyeon AHN <suahn@e1r11p3.clusters.42paris.fr>
2022-12-21 21:25:45 +0100

    fix

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-WP_CONFIG= "/var/www/html/wp-config.php"
-if  [ ! -f "$WP_CONFIG" ]; then
-    #chek if it's installed
-    wp --info
-    #download the latest version of the WordPress core files.
-    wp core download --allow-root
-    #create the wp-config.php file
-    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=$WORDPRESS_TABLE_PREFIX --allow-root
-    #wp db create
+WP_USER= "/var/www/html/users"
+if  [ ! -f "$WP_USER" ]; then
     wp core install --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
     wp user create $WORDPRESS_USER "https://suahn.42.fr" --user_pass=$WORDPRESS_USER_PASS --role=Default --allow-root
+    touch users
 fi
