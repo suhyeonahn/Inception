d11d45d1d774ad6b65313988280e1f1166668a4f
Suhyeon AHN <suahn@e1r11p3.clusters.42paris.fr>
2022-12-21 19:36:44 +0100

    add wp user from mysql

--- srcs/requirements/mariadb/conf/setup.sql
+++ srcs/requirements/mariadb/conf/setup.sql
@@ -4,3 +4,7 @@ GRANT ALL PRIVILEGES ON db.* TO 'wp'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root_pass';
+
+USE wordpress;
+INSERT INTO `wp_users` (`user_login`, `user_pass`, `user_email`, `user_registered`, `user_status`)
+VALUES('$WORDPRESS_ADMIN_USER', '$WORDPRESS_ADMIN_PASS', 'agent@example.com', '2022-12-21 16:34:15', 0);
