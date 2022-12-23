bfb62f3e1a5779a6d16bb7ac0168df663d3b00d3
Suhyeon AHN <suahn@e1r6p6.clusters.42paris.fr>
2022-12-23 12:14:01 +0100

    fix err wp

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-WP_CONFIG= "/var/www/html/WP_CONFIG"
-if  [ ! -f "$WP_CONFIG" ]; then
+#WP_CONFIG= "/var/www/html/WP_CONFIG"
+if  [ ! -f "WP_CONFIG" ]; then
     wp core install --url=$WORDPRESS_URI --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role='editor' --allow-root
     touch WP_CONFIG
