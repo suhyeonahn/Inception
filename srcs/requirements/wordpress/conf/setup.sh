1e888f402f95cc8daf48df867b6235ee2cfd436f
Suhyeon AHN <54574221+suhyeonahn@users.noreply.github.com>
2022-12-27 21:02:14 +0100

    Update setup.sh

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#if  [ ! -f "/var/www/html/wp-config.php" ]; then
+if  [ ! -f "/var/www/html/wp-config.php" ]; then
     #wp plugin install insert-headers-and-footers --activate
     wp core download --allow-root
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --path="/var/www/html/" --allow-root --skip-check --extra-php <<PHP
@@ -11,4 +11,4 @@
     wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role='editor' --path="/var/www/html/" --allow-root
     touch WP_CONFIG
-#fi
+fi
